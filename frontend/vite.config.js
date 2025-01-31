b4bc34753b13fc3e651d06fe31ad2ee50d0e9a40
Dheeraj <ydheeraj2003@example.com>
2025-01-31 19:20:22 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port : 5000,
     proxy: {
       "/api" : {
-        target : "http://localhost:8000",
+        target : "https://twitter-backend-61fc.onrender.com/",
         changeOrigin : true
       }
     }
